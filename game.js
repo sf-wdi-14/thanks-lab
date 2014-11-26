ce35da5c25fc76f93908a11ba24c1e9bc245c966
Protosac <proto@sacquorum.com>
2014-11-26 10:24:11 -0800

    Add shuffle function

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 // Page load
 window.onload = function() {
+
 	var boxes = document.querySelectorAll(".box"),
 		board = document.querySelector(".board"),
 		startButton = document.querySelector("#start"),
@@ -8,16 +9,19 @@ window.onload = function() {
 				 "e","f","g","h",
 				 "a","b","c","d",
 				 "e","f","g","h"];
+
+	function shuffle(o) {
+		for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j]= x);
+    	return o;
+	};
+	
 	
 	startButton.onclick = function() {
+		newBoard = shuffle(tiles);
+		console.log(newBoard);
 		for (var n = 0; n < boxes.length; n++) {
-			var i = Math.floor((Math.random() * boxes.length) );
-			console.log(i);
-			if (!boxes[i].innerHTML) {
-				boxes[n].innerHTML = tiles[i];
-				console.log(tiles[i], boxes[i].innerHTML);
-			} 
-		}
+			boxes[n].innerHTML = tiles[n];
+		} 
 	}
 	
 }
