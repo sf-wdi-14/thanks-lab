1742612717117c91d8c664d2cae1882c2907fbb9
Protosac <proto@sacquorum.com>
2014-11-30 08:54:35 -0800

    Clean up code

--- game.js
+++ game.js
@@ -1,16 +1,18 @@
-// Page load
 window.onload = function() {
 
 	var boxes = document.querySelectorAll(".box"),
 		board = document.querySelector(".board"),
 		startButton = document.querySelector("#start"),
 		stopButton = document.querySelector("#stop"),
-		notification = document.querySelector(".message");
+		notification = document.querySelector(".message"),
+		counter = document.querySelector("#counter");
 	
-	var tiles = ["a","b","c","d",
-				 "e","f","g","h",
-				 "a","b","c","d",
-				 "e","f","g","h"];
+	var tiles = [
+		"a","b","c","d",
+		"e","f","g","h",
+		"a","b","c","d",
+		"e","f","g","h"
+		];
 
 	// Shuffle the tiles, inspired by Fisher-Yates 
 	// http://en.wikipedia.org/wiki/Fisher_Yates_shuffle
@@ -22,14 +24,14 @@ window.onload = function() {
 	// No game until Start button is pressed
 	var started = false,
 		  newBoard = [],
-		  currentBoard = [];
+		  score = 0;
 
 	// Initiate a new game board
 	startButton.onclick = function() {
 		newBoard = shuffle(tiles);
 		started = true;
 		startButton.disabled = true;
-		console.log(newBoard);	
+		startButton.classList.add('dim');	
 		for (var n = 0; n < boxes.length; n++) {
 			boxes[n].innerHTML = newBoard[n];
 		} 
@@ -39,8 +41,11 @@ window.onload = function() {
 	stopButton.onclick = function() {
 		for (var b = 0; b < boxes.length; b++) {
 			boxes[b].innerHTML = "";
+			boxes[b].classList.remove('reveal','noclick','match');
 			notification.innerHTML = "Yummy ...a clean board.";
 			startButton.disabled = false;
+			pairs = 0, score = 0, counter = 0;
+			cards = [];
 		}
 	}
 
@@ -50,16 +55,16 @@ window.onload = function() {
 
 	function isMatch() {
 		if (cards[0].innerHTML === cards[1].innerHTML) {
-			cards[0].classList.add('match');
-			cards[1].classList.add('match');
+			cards[0].classList.add('match','noclick');
+			cards[1].classList.add('match','noclick');
 			cards = [];
 			notification.innerHTML = "Match!";
 			pairs++;
 		} else {
 			setTimeout(function()
 			  {
-			    cards[0].classList.remove('reveal');
-			    cards[1].classList.remove('reveal');
+			    cards[0].classList.remove('reveal','noclick');
+			    cards[1].classList.remove('reveal','noclick');
 			    cards = [];
 			  }, 500);
 		}
@@ -73,22 +78,26 @@ window.onload = function() {
 	}
 	
 	// When player clicks a box ...
+		var attempts = 0;
   for (var t = 0; t < boxes.length; t++) {
-		
 		boxes[t].onclick = function() {
 			if (started === true) {
-				this.classList.add('reveal');
-			  cards.push(this);		  
+				attempts++;
+				this.classList.add('reveal','noclick');
+			  cards.push(this);	
+			  counter.innerHTML = attempts;	  
   	    if (cards.length === 2) {
 			    isMatch();				 
 		    }		    
 			} else {
 				notification.innerHTML = "Press the start button.";
+				notification.classList.add('flash');
 			}
+
 			gameOver();
 		}
 	}
 }
 // Current issues:
 // 1. Clicking the same card twice to get an auto match.
-// 2. 
+// 2. "Covering" a card
